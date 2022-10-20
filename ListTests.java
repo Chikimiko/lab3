c927422422c67db914a7dc365018305261b0b7b6
Chikimiko <cococandy333@gmail.com>
2022-10-20 10:22:47 -0700

    work

--- ListTests.java
+++ ListTests.java
@@ -21,10 +21,32 @@ public class ListTests {
     List<String> list1 = new ArrayList<>();
     list1.add("cook");
     list1.add("ate");
+    list1.add("cookies");
+    list1.add("ate");
+    list1.add("cookie");
     List<String> solution = new ArrayList<>();
     solution.add("cook");
+    solution.add("cookies");
+    solution.add("cookie");
     assertEquals(solution, ListExamples.filter(list1, new StringCheckerClass()));
   }
 
+  @Test
+  public void test3() {
+    List<String> list1 = new ArrayList<>();
+    list1.add("cook");
+    list1.add("ate");
+    list1.add("cookies");
+    List<String> list2 = new ArrayList<>();
+    list1.add("and");
+    list1.add("milk");
+    List<String> solution = new ArrayList<>();
+    solution.add("cook");
+    solution.add("ate");
+    solution.add("cookies");
+    solution.add("and");
+    solution.add("milk");
+    assertEquals(solution, ListExamples.merge(list1, list2));
+  }
 
 }
